bf49856988ee6e5aa879bd712ed3ede831219f9c
JoshuaDevinney <jzd278@psu.edu>
2021-11-22 15:26:45 -0500

    Added Orbit Controls, GridHelper, Ambient Light, and Earth placeholder mesh. Implemented animate function to add interactivity. This is a baseline for working on the environment going forward.

---
+++ SeniorDesign/main.js
@@ -0,0 +1,41 @@
+import './style.css'
+
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg')
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
+
+const earthGeo = new THREE.SphereGeometry(3, 32, 32);
+const earthMaterial = new THREE.MeshStandardMaterial( {color: 0x30D5C8});
+const earth = new THREE.Mesh(earthGeo, earthMaterial);
+scene.add(earth);
+
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+scene.add(ambientLight);
+
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(gridHelper)
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+    requestAnimationFrame( animate );
+
+    controls.update();
+
+    renderer.render(scene, camera);
+}
+
+animate();
