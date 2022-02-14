2dbb7624641a89423ad32202e707e80e171e1193
JoshuaDevinney <jzd278@psu.edu>
2022-02-14 12:01:58 -0500

    Enabled alpha and antialias in renderer

--- SeniorDesign/main.js
+++ SeniorDesign/main.js
@@ -16,7 +16,7 @@ const scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
-const renderer = new THREE.WebGLRenderer({
+const renderer = new THREE.WebGLRenderer({ alpha:true, antialias:true,
     canvas: document.querySelector('#bg')
 });
 
