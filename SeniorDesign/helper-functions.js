c3edd4bbd4613e2c4dab85a1bc7e2e324497e63a
JoshuaDevinney <jzd278@psu.edu>
2022-02-17 22:14:14 -0500

    Added New Helper Functions. Added createMesh, createSTL, attachCard, and createCard. Defined createMesh and createSTL. Modified createPlanet.

--- SeniorDesign/helper-functions.js
+++ SeniorDesign/helper-functions.js
@@ -1,7 +1,8 @@
 const THREE = require('three');
+const {STLLoader} = require("three/examples/jsm/loaders/STLLoader");
 
 //get physical specifications from user and create a planet with them
-function addPlanet( radius, widthSegments, heightSegments, xPosition, yPosition, zPosition ){
+function createPlanet( radius, widthSegments, heightSegments, xPosition, yPosition, zPosition, material){
 
     //check that radius is greater than zero
     if(radius <= 0)
@@ -9,10 +10,9 @@ function addPlanet( radius, widthSegments, heightSegments, xPosition, yPosition,
 
     //create initial geometry and material
     let planetGeo = new THREE.SphereGeometry(radius, widthSegments, heightSegments);
-    let planetMaterial = new THREE.MeshStandardMaterial( {color: "blue"});
 
     //create mesh out of geometry and material
-    let planet = new THREE.Mesh(planetGeo, planetMaterial);
+    let planet = new THREE.Mesh(planetGeo, material);
 
     //set specified position
     planet.position.set(xPosition, yPosition, zPosition);
@@ -21,6 +21,54 @@ function addPlanet( radius, widthSegments, heightSegments, xPosition, yPosition,
     return planet;
 }
 
+function createMesh(type, material){
+    let planetMaterial = null;
+
+    if(type == 'texture'){
+        planetMaterial = new THREE.MeshStandardMaterial({map: material});
+    }
+    else if(type == 'color'){
+        planetMaterial = new THREE.MeshStandardMaterial({color: material});
+    }
+    else{
+        planetMaterial = new THREE.MeshBasicMaterial({color: material});
+    }
+
+    return planetMaterial;
+}
+
+function createSTL(path, xPos, yPos, zPos, material){
+    const loader = new STLLoader();
+    let stlMesh = null;
+    loader.load(
+        path,
+        function (geometry) {
+            stlMesh = new THREE.Mesh(geometry, material);
+            stlMesh.position.set(xPos, yPos, zPos);
+        },
+        (xhr) => {
+            console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+        },
+        (error) => {
+            console.log(error);
+        }
+    )
+
+    return stlMesh;
+}
+
+function attachCard(){
+
+}
+
+function createCard(){
+
+}
+
+
+
 module.exports = {
-    addPlanet
+    createSTL,
+    createPlanet,
+    createMesh
 }
