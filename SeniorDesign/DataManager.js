74d66082c0bd09dde1c06adef9a556e90267ef76
DMyers5150 <dom5378@psu.edu>
2022-04-21 20:36:23 -0400

    Testing for new raycast

--- SeniorDesign/DataManager.js
+++ SeniorDesign/DataManager.js
@@ -292,7 +292,7 @@ const spacecraftImages = ["Resources/Images/spacecraftFact1.JPG",
 
 planets.get("Spacecraft").initializeFactCards(spacecraftFacts, spacecraftImages);
 
-const raycastModifier = 0;
+const raycastModifier = 0.5;
 
 //-----Handle click function using raycasts
 function checkPlanetClick(event){
@@ -300,7 +300,7 @@ function checkPlanetClick(event){
     //get location of mouse and use it to set the raycast
     //extra math is to normalize coordinates to user's screen
     if(renderer.xr.isPresenting){
-        rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1 - raycastModifier/2);
+        rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1 - raycastModifier);
     }
     else{
         rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1);
@@ -683,7 +683,7 @@ scene.add(moonOrbit);
 
 const cameraHolder = new THREE.Group();
 cameraHolder.add(camera);
-cameraHolder.position.set(0, 1, 0);
+cameraHolder.position.set(0, raycastModifier, 0);
 scene.add(cameraHolder);
 
 function animate(){
