25bd66e7fd740a4e1f256c77e6a3376ab2266356
JoshuaDevinneyPSU <80540937+JoshuaDevinneyPSU@users.noreply.github.com>
2022-04-21 21:39:40 -0400

    updated raycast function to original

--- SeniorDesign/DataManager.js
+++ SeniorDesign/DataManager.js
@@ -304,7 +304,7 @@ function checkPlanetClick(event){
     //get location of mouse and use it to set the raycast
     //extra math is to normalize coordinates to user's screen
     if(renderer.xr.isPresenting){
-        rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1/2);
+        rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1);
     }
     else{
         rayPointer.set((event.clientX / window.innerWidth) * 2 - 1, -((event.clientY) / window.innerHeight) * 2 + 1);
